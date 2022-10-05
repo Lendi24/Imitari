902e813bef6cf82c2445990afaf4dcc42dfcc404
Jonathan <jonathan.nilsson4@elev.ga.ntig.se>
2022-10-05 11:11:57 +0200

    test

---
+++ public/js/main.js
@@ -0,0 +1,65 @@
+"use strict";
+class Pixel {
+    constructor() {
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
+        this.a = 0;
+    }
+    getRGBA() {
+        return {
+            r: this.r,
+            g: this.g,
+            b: this.b,
+            a: this.a,
+        };
+    }
+    setRGBA(r, g, b, a) {
+        this.r = Util.clamp(r, 255, 0);
+        this.g = Util.clamp(g, 255, 0);
+        this.b = Util.clamp(b, 255, 0);
+        this.b = Util.clamp(a, 255, 0);
+    }
+}
+class CanvasDraw {
+    constructor(width, height) {
+        this.targetFPS = 30;
+        this.drawing = [];
+        for (let x = 0; x < width; x++) {
+            this.drawing[x] = [];
+            for (let y = 0; y < height; y++) {
+                this.drawing[x][y] = new Pixel();
+            }
+        }
+        let obj = this;
+        setInterval(function () { obj.drawToCanvas(obj.drawing); }, 1000 / this.targetFPS);
+    }
+    drawToCanvas(drawing) {
+        let canvas = document.getElementById("drawing-area");
+        let pixelSize = 5;
+        if (canvas.parentElement != null) {
+            canvas.width = canvas.parentElement.clientWidth;
+            canvas.height = canvas.parentElement.clientHeight;
+            pixelSize = (Math.min(canvas.parentElement.clientHeight / drawing[0].length, canvas.parentElement.clientWidth / drawing.length));
+            console.log(pixelSize);
+        }
+        let ctx = canvas.getContext("2d");
+        let pixelGapSize = 1;
+        for (let x = 0; x < drawing.length; x++) {
+            for (let y = 0; y < drawing[x].length; y++) {
+                ctx.beginPath();
+                ctx.moveTo(x * pixelSize + pixelGapSize, y * pixelSize + pixelGapSize);
+                ctx.lineTo(x * pixelSize + pixelSize - pixelGapSize, y * pixelSize + pixelGapSize);
+                ctx.lineTo(x * pixelSize + pixelSize - pixelGapSize, y * pixelSize + pixelSize - pixelGapSize);
+                ctx.lineTo(x * pixelSize + pixelGapSize, y * pixelSize + pixelSize - pixelGapSize);
+                ctx.lineTo(x * pixelSize + pixelGapSize, y * pixelSize + pixelGapSize);
+                ctx.fillStyle = "rgb(" + drawing[x][y].getRGBA().r + ", " + drawing[x][y].getRGBA().g + ", " + drawing[x][y].getRGBA().b + ")";
+                ctx.fill();
+            }
+        }
+    }
+}
+class Util {
+}
+Util.clamp = (num, max, min) => Math.min(Math.max(num, min), max);
+let x = new CanvasDraw(20, 10);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
   },
