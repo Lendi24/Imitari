c0c3ac8fe9a77e2b60b396dc3b2d893478161ede
Lendi24 <Lendic24@hotmail.com>
2022-10-10 18:38:54 +0200

    fixed place pixel offset when using zoom

--- public/js/general.js
+++ public/js/general.js
@@ -4,5 +4,5 @@ class Util {
 Util.clamp = (num, max, min) => Math.min(Math.max(num, min), max);
 Util.screenToCordX = (cord) => Util.clamp(Util.screenToCord(cord - DrawView.jsCanvas.getBoundingClientRect().x), DrawView.getLayer(0).drawing.length - 1, 0);
 Util.screenToCordY = (cord) => Util.clamp(Util.screenToCord(cord - DrawView.jsCanvas.getBoundingClientRect().y), DrawView.getLayer(0).drawing[0].length - 1, 0);
-Util.screenToCord = (cord) => Math.floor(cord / DrawView.pixelSize);
+Util.screenToCord = (cord) => Math.floor(cord / (DrawView.pixelSize * DrawView.zoom));
 DrawView.init(100, 80);
--- src/ts/general.ts
+++ src/ts/general.ts
@@ -3,7 +3,7 @@ class Util {
     public  static clamp = (num : number, max : number, min : number,) => Math.min(Math.max(num, min), max);
     public  static screenToCordX = (cord : number) => Util.clamp(Util.screenToCord(cord - DrawView.jsCanvas.getBoundingClientRect().x), DrawView.getLayer(0).drawing.length    -1, 0); 
     public  static screenToCordY = (cord : number) => Util.clamp(Util.screenToCord(cord - DrawView.jsCanvas.getBoundingClientRect().y), DrawView.getLayer(0).drawing[0].length -1, 0); 
-    private static screenToCord  = (cord : number) => Math.floor(cord / DrawView.pixelSize); 
+    private static screenToCord  = (cord : number) => Math.floor(cord / (DrawView.pixelSize * DrawView.zoom)); 
 }
 
 DrawView.init(100,80);
