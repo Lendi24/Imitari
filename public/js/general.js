f935491f6e77a4343780576e677e063854a1777e
Lendi24 <Lendic24@hotmail.com>
2022-10-10 11:53:26 +0200

    fixed "screenToPixel" for un-even sides

--- public/js/general.js
+++ public/js/general.js
@@ -2,7 +2,7 @@
 class Util {
 }
 Util.clamp = (num, max, min) => Math.min(Math.max(num, min), max);
-Util.screenToCordX = (cord) => Util.clamp(Util.screenToCord(cord - (window.innerWidth - DrawView.jsCanvas.clientWidth) / 2), DrawView.getLayer(0).drawing.length - 1, 0);
-Util.screenToCordY = (cord) => Util.clamp(Util.screenToCord(cord - (window.innerHeight - DrawView.jsCanvas.clientHeight) / 2), DrawView.getLayer(0).drawing[0].length - 1, 0);
+Util.screenToCordX = (cord) => Util.clamp(Util.screenToCord(cord - DrawView.jsCanvas.getBoundingClientRect().x), DrawView.getLayer(0).drawing.length - 1, 0);
+Util.screenToCordY = (cord) => Util.clamp(Util.screenToCord(cord - DrawView.jsCanvas.getBoundingClientRect().y), DrawView.getLayer(0).drawing[0].length - 1, 0);
 Util.screenToCord = (cord) => Math.floor(cord / DrawView.pixelSize);
 DrawView.init(100, 80);
