3dfade4eb57885d256fc06e22284adb453bbefce
Lendi24 <Lendic24@hotmail.com>
2022-10-13 14:36:31 +0200

    moved stuff

--- public/js/events.js
+++ public/js/events.js
@@ -38,92 +38,13 @@ window.onwheel = function (e) {
     }
 };
 window.onload = function () {
-    let classes = ["text-white", "border-2", "invert", "rounded", "hover:bg-green-700", "hover:scale-110", "transform", "transition-all", "mdi"];
-    let htmlTools = (document.getElementById("tool-section"));
-    for (let tool in tools) {
-        let newHTML = tools[tool].html = document.createElement("span");
-        newHTML.onclick = function () { switchTool(tool); };
-        classes.forEach(classStr => {
-            newHTML.classList.add(classStr);
-        });
-        newHTML.classList.add(tools[tool].icon);
-        htmlTools.appendChild(newHTML);
-        tools[tool].html = newHTML;
-    }
-    switchTool("b");
-    let topBarHtml = (document.getElementById("top-bar"));
-    for (let item in topBar) {
-        let dropdown = (document.createElement("drop"));
-        let dropname = (document.createElement("button"));
-        let dropcont = (document.createElement("drop-content"));
-        dropname.innerHTML = item;
-        for (let i = 0; i < topBar[item].length; i++) {
-            let dropitemscont = document.createElement("item-section");
-            for (let innerItem in topBar[item][i]) {
-                let innerItemHTML = document.createElement("item");
-                innerItemHTML.innerText = innerItem;
-                innerItemHTML.onmousedown = topBar[item][i][innerItem];
-                dropitemscont.appendChild(innerItemHTML);
-            }
-            dropcont.appendChild(dropitemscont);
-        }
-        dropdown.appendChild(dropname);
-        dropdown.appendChild(dropcont);
-        topBarHtml.appendChild(dropdown);
-    }
+    uiLoader.loadTools();
+    uiLoader.loadTopMenu();
 };
 function updateUIPos() {
     (document.getElementById("posX")).innerText = Util.screenToCordX(CustomMouseEvent.mouseX).toString();
     (document.getElementById("posY")).innerText = Util.screenToCordY(CustomMouseEvent.mouseY).toString();
 }
-let topBar = {
-    "File": [
-        {
-            "New": function () { console.log("New"); },
-            "Open": function () { console.log("New"); },
-            "Save": Object,
-        },
-        {
-            "Import": Object,
-            "Export": Object,
-        }
-    ],
-    "Edit": [
-        {
-            "Undo": Object,
-            "Redo": Object,
-        },
-    ],
-    "Layer": [
-        {
-            "Create layer": Object,
-            "Remove layer": Object,
-        },
-        {
-            "Clear": Object,
-        }
-    ],
-    "Settings": [
-        {
-            "Theme": Object,
-            "Zoom": Object,
-            "Offset": Object,
-            "Grid": Object,
-            "Render": Object,
-        },
-    ],
-    "About": [
-        {
-            "Imitari": function () { new CustomWindow("re", "http://127.0.0.1:5500/public/html/windows/top-bar/about/Imitari.html"); },
-        },
-    ],
-};
-let tools = {
-    "b": { obj: new DrawTool(), html: "", icon: "mdi-brush" },
-    "l": { obj: new LineTool(), html: "", icon: "mdi-pencil-ruler" },
-    "f": { obj: new FillTool(), html: "", icon: "mdi-format-color-fill" },
-    "s": { obj: new ShapeTool(), html: "", icon: "mdi-shape" },
-};
 function switchTool(val) {
     try {
         let tool = tools[val];
--- src/ts/events.ts
+++ src/ts/events.ts
@@ -55,56 +55,9 @@ window.onwheel = function(e:WheelEvent) {
 
 
 window.onload = function() {
+    uiLoader.loadTools();
+    uiLoader.loadTopMenu();
 
-    //Update UI: Tools
-    let classes = ["text-white", "border-2", "invert", "rounded", "hover:bg-green-700", "hover:scale-110", "transform", "transition-all", "mdi"]
-    let htmlTools = <HTMLElement>(document.getElementById("tool-section"));
-
-    for (let tool in tools) {
-        let newHTML = tools[tool].html = document.createElement("span");
-        
-        newHTML.onclick = function () {switchTool(tool);}
-        classes.forEach(classStr => {
-            newHTML.classList.add(classStr);
-        });
-
-        newHTML.classList.add(tools[tool].icon);
-
-        htmlTools.appendChild(newHTML);
-        tools[tool].html = newHTML;        
-    }
-
-    switchTool("b");
-
-    //Update UI: Top-bar
-    let topBarHtml = <HTMLElement>(document.getElementById("top-bar"));
-    for (let item in topBar) {
-        let dropdown = <HTMLElement>(document.createElement("drop"));
-        let dropname = <HTMLElement>(document.createElement("button"));
-        let dropcont = <HTMLElement>(document.createElement("drop-content"));
-
-        dropname.innerHTML = item;
-
-
-        for (let i = 0; i < topBar[item].length; i++) {
-            let dropitemscont = document.createElement("item-section");
-
-            for (let innerItem in topBar[item][i]) {
-                let innerItemHTML = document.createElement("item");
-                innerItemHTML.innerText = innerItem;
-                innerItemHTML.onmousedown = topBar[item][i][innerItem];
-                dropitemscont.appendChild(innerItemHTML);
-                //const element = array[index];
-            }
-
-            dropcont.appendChild(dropitemscont);
-        }
-
-        dropdown.appendChild(dropname);
-        dropdown.appendChild(dropcont);
-
-        topBarHtml.appendChild(dropdown);
-    }
 }
 
 function updateUIPos() {    
@@ -112,68 +65,6 @@ function updateUIPos() {
     (<HTMLElement>(document.getElementById("posY"))).innerText = Util.screenToCordY(CustomMouseEvent.mouseY).toString();
 }
 
-let topBar: {[key : string]: any} = {
-    "File"      : [
-        {
-            "New"           : function () { console.log("New"); },
-            "Open"          : function () { console.log("New"); },
-            "Save"          : Object,
-        },
-
-
-        {
-            "Import"        : Object,
-            "Export"        : Object,    
-        }
-    ],
-
-    "Edit"      : [
-        {
-            "Undo"           : Object,
-            "Redo"           : Object,
-        },
-    ],
-
-    "Layer"      : [
-        {
-            "Create layer"  : Object,
-            "Remove layer"  : Object,
-        },
-
-        {
-            "Clear"         : Object,
-        }
-],
-/*
-    "Tools"      : [
-        {
-            "Tool"          : Object,
-        },
-],*/
-
-    "Settings"      : [
-        {
-            "Theme"         : Object,
-            "Zoom"          : Object,
-            "Offset"        : Object,
-            "Grid"          : Object,
-            "Render"        : Object,
-        },
-],
-
-    "About"      : [
-        {
-            "Imitari"       : function() {new CustomWindow("re", "http://127.0.0.1:5500/public/html/windows/top-bar/about/Imitari.html"); },
-        },
-],
-}
-
-let tools: {[key: string]: any} = { 
-    "b" : {obj : new DrawTool(),  html : "", icon : "mdi-brush"},
-    "l" : {obj : new LineTool(),  html : "", icon : "mdi-pencil-ruler"},
-    "f" : {obj : new FillTool(),  html : "", icon : "mdi-format-color-fill"},
-    "s" : {obj : new ShapeTool(), html : "", icon : "mdi-shape"},
-};
 
 function switchTool(val:string) {
     try {
