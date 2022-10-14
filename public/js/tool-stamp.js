a0970d2e0de285a1719ede5185d0a551555e9aa2
Lendi24 <Lendic24@hotmail.com>
2022-10-14 10:43:09 +0200

    redid stamps array

--- public/js/tool-stamp.js
+++ public/js/tool-stamp.js
@@ -3,22 +3,45 @@ class StampTool extends Tool {
     constructor() {
         super(...arguments);
         this.stamps = [
-            [new Pixel(0, 0, 0, 255), new Pixel(255, 255, 255, 255), new Pixel(255, 255, 255, 255), new Pixel(0, 0, 0, 255),],
-            [new Pixel(255, 255, 255, 255), new Pixel(0, 0, 0, 255), new Pixel(0, 0, 0, 255), new Pixel(255, 255, 255, 255),],
-            [new Pixel(255, 255, 255, 255), new Pixel(255, 255, 255, 255), new Pixel(255, 255, 255, 255), new Pixel(255, 255, 255, 255),],
-            [new Pixel(255, 255, 255, 255), new Pixel(0, 0, 0, 255), new Pixel(0, 0, 0, 255), new Pixel(255, 255, 255, 255),],
-            [new Pixel(255, 255, 255, 255), new Pixel(0, 0, 0, 255), new Pixel(0, 0, 0, 255), new Pixel(255, 255, 255, 255),],
+            [
+                new Pixel(0, 0, 0, 255),
+                new Pixel(255, 255, 255, 255),
+                new Pixel(255, 255, 255, 255),
+                new Pixel(255, 255, 255, 255),
+                new Pixel(255, 255, 255, 255),
+            ],
+            [
+                new Pixel(255, 255, 255, 255),
+                new Pixel(0, 0, 0, 255),
+                new Pixel(255, 255, 255, 255),
+                new Pixel(0, 0, 0, 255),
+                new Pixel(0, 0, 0, 255),
+            ],
+            [
+                new Pixel(255, 255, 255, 255),
+                new Pixel(0, 0, 0, 255),
+                new Pixel(255, 255, 255, 255),
+                new Pixel(0, 0, 0, 255),
+                new Pixel(0, 0, 0, 255),
+            ],
+            [
+                new Pixel(0, 0, 0, 255),
+                new Pixel(255, 255, 255, 255),
+                new Pixel(255, 255, 255, 255),
+                new Pixel(255, 255, 255, 255),
+                new Pixel(255, 255, 255, 255),
+            ],
         ];
     }
     onMouse(event) {
         if (CustomMouseEvent.mouseLeftDown && CustomMouseEvent.mouseLeftChanged) {
+            console.log(this.stamps);
             this.blit(this.stamps, DrawView.getLayer(0).drawing, 0, 0, 4, 5, Util.screenToCordX(CustomMouseEvent.mouseX), Util.screenToCordY(CustomMouseEvent.mouseY));
         }
     }
     blit(srcRef, dstRef, sx, sy, sw, sh, dx, dy) {
-        for (let x = 0; x < srcRef.length; x++) {
-            for (let y = 0; y < srcRef[x].length; y++) {
-                console.log(Util.screenToCordX(dx));
+        for (let x = sx; x < sw; x++) {
+            for (let y = sy; y < sh; y++) {
                 dstRef[x + dx][y + dy].r = srcRef[x][y].r;
                 dstRef[x + dx][y + dy].g = srcRef[x][y].g;
                 dstRef[x + dx][y + dy].b = srcRef[x][y].b;
--- src/ts/tool-stamp.ts
+++ src/ts/tool-stamp.ts
@@ -1,22 +1,57 @@
 class StampTool extends Tool {
+    /*
     stamps = [
-        [ new Pixel(0, 0, 0, 255),        new Pixel(255, 255, 255, 255), new Pixel(255, 255, 255, 255), new Pixel(0, 0, 0, 255), ],
+        [ new Pixel(0, 0, 0, 255),        new Pixel(255, 255, 255, 255), new Pixel(255, 255, 255, 255), new Pixel(0, 0, 0, 255),       ],
         [ new Pixel(255, 255, 255, 255),  new Pixel(0, 0, 0, 255),       new Pixel(0, 0, 0, 255),       new Pixel(255, 255, 255, 255), ],
         [ new Pixel(255, 255, 255, 255),  new Pixel(255, 255, 255, 255), new Pixel(255, 255, 255, 255), new Pixel(255, 255, 255, 255), ],
         [ new Pixel(255, 255, 255, 255),  new Pixel(0, 0, 0, 255),       new Pixel(0, 0, 0, 255),       new Pixel(255, 255, 255, 255), ],
         [ new Pixel(255, 255, 255, 255),  new Pixel(0, 0, 0, 255),       new Pixel(0, 0, 0, 255),       new Pixel(255, 255, 255, 255), ],
+    ]*/
+
+    stamps = [
+        [
+            new Pixel(0, 0, 0, 255),       
+            new Pixel(255, 255, 255, 255), 
+            new Pixel(255, 255, 255, 255), 
+            new Pixel(255, 255, 255, 255), 
+            new Pixel(255, 255, 255, 255), 
+        ],
+
+        [
+            new Pixel(255, 255, 255, 255),
+            new Pixel(0, 0, 0, 255),      
+            new Pixel(255, 255, 255, 255),
+            new Pixel(0, 0, 0, 255),      
+            new Pixel(0, 0, 0, 255),      
+        ],
+
+        [
+            new Pixel(255, 255, 255, 255), 
+            new Pixel(0, 0, 0, 255),       
+            new Pixel(255, 255, 255, 255), 
+            new Pixel(0, 0, 0, 255),       
+            new Pixel(0, 0, 0, 255),       
+        ],
+
+        [
+            new Pixel(0, 0, 0, 255),       
+            new Pixel(255, 255, 255, 255), 
+            new Pixel(255, 255, 255, 255), 
+            new Pixel(255, 255, 255, 255), 
+            new Pixel(255, 255, 255, 255), 
+        ],
     ]
 
     onMouse(event : CustomMouseEvent) {
         if (CustomMouseEvent.mouseLeftDown && CustomMouseEvent.mouseLeftChanged) { 
+            console.log(this.stamps)
             this.blit(this.stamps, DrawView.getLayer(0).drawing, 0, 0, 4, 5, Util.screenToCordX(CustomMouseEvent.mouseX), Util.screenToCordY(CustomMouseEvent.mouseY) )
         }
     }
 
     blit(srcRef : Array<Array<Pixel>>, dstRef : Array<Array<Pixel>>, sx : number, sy : number, sw : number, sh :number, dx : number, dy : number, ) {
-        for (let x = 0; x < srcRef.length; x++) {
-            for (let y = 0; y < srcRef[x].length; y++) {
-                console.log(Util.screenToCordX(dx))
+        for (let x = sx; x < sw; x++) {
+            for (let y = sy; y < sh; y++) {
                 dstRef[x+dx][y+dy].r = srcRef[x][y].r;
                 dstRef[x+dx][y+dy].g = srcRef[x][y].g;
                 dstRef[x+dx][y+dy].b = srcRef[x][y].b;
